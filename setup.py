99d00053ac7c7634600269d6e91f5dbbec43b86c
Artiprocher <wangye87v5@hotmail.com>
2022-06-15 16:49:43 +0800

    update

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oass",
+    version="1.0.0",
+    author="Artiprocher",
+    author_email="zjduan@stu.ecnu.edu.cn",
+    description="Optimal Action Space Search (OASS) is an algorithm for path planning problems on directed acyclic graphs (DAG) based on reinforcement learning (RL) theory.",
+    url="https://github.com/ECNU-CILAB/OASS", 
+    packages=find_packages()
+)
